26762f1caea8a4a9207b28b3b790421b6ff984ef
Naveenk0911 <naveenkumar092004@gmail.com>
2025-09-08 20:25:04 +0530

    ..

---
+++ fundamnetal-practise-problems/Area_of_circle.java
@@ -0,0 +1,14 @@
+package fundamnetal-practise-problems;
+
+import java.util.*;
+public class Area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter radius :");
+        float r=sc.nextFloat();
+        float area =(float)3.14*r*r;
+        System.out.println("area of circle :"+area);
+        sc.close();
+
+    }
+}
