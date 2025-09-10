197f89e6d8edba1c094e64b01d8ded650ccec9d9
Naveenk0911 <naveenkumar092004@gmail.com>
2025-09-10 22:33:42 +0530

    ..

---
+++ STRINGS/Level1/StringIndexOutDemo.java
@@ -0,0 +1,25 @@
+package STRINGS.Level1;
+import java.util.Scanner;
+
+public class StringIndexOutDemo {
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length()));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length())); 
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Caught Exception: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        handleException(text);
+        sc.close();
+    }
+}
